735126aa831fcfe30bf2acb3de286604966d4c0d
徐刘根 <1573876303@qq.com>
2015-10-06 21:27:07 +0800

    提交代码

---
+++ src/cn/itcast/ssm/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package cn.itcast.ssm.mapper;
+
+import cn.itcast.ssm.po.User;
+import cn.itcast.ssm.po.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
